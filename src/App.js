1bf1d75bd829036469e86f274d7d901cd07dcbd5
MitchRose976 <mitch.j.rose@outlook.com>
2021-12-19 23:07:36 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
+import background from './media/arena-background.jpg';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" styles={{backgroundImage:`media/nhl-wallpaper.jpg`}}>
+      <img src={background} className="background-image"/>
+      
     </div>
   );
 }
