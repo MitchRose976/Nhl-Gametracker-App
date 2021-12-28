362b60c59fa0d95bd4649d6d1ae07539ce2726d5
MitchRose976 <mitch.j.rose@outlook.com>
2021-12-28 10:05:23 -0500

    fixed problem retrieving and displaying player info and stats from NHL API

--- src/components/RosterTable.js
+++ src/components/RosterTable.js
@@ -6,28 +6,29 @@ import Container from "../UX/Container";
 
 const RosterTable = () => {
   // This variable is controlled by state and will change to the id of whatever team the user submits
-  let teamIndex = 9;
+  let teamIndex = 10;
   let url = `https://statsapi.web.nhl.com/api/v1/teams/${teamIndex}?expand=team.roster`;
   // State to hold each player bio (height, weight, #, birthplace, birthdate etc.)
   const [playerInfo, setPlayerInfo] = useState(null);
   // State to hold all players stats (games played, goals, assists, hits, blocked shots etc.)
   const [playerStats, setPlayerStats] = useState(null);
-  const [playerData, setplayerData] = useState(null);
+  //const [playerData, setplayerData] = useState(null);
   // current season
   let currentYear = new Date().getFullYear();
   let currentSeason = currentYear.toString().concat(currentYear + 1);
   // 1st argument - function to run when monitored variable changes
   // 2nd argument - variable to monitor
   // Fetch API from NHL
+  let playerData;
+
   useEffect(() => {
     let result;
     let playerInfoPromises = [];
     let playerStatsPromises = [];
-    axios.get(url)
-    .then((response) => {
+    axios.get(url).then((response) => {
       // Destructure promise to get the team roster
       result = response.data.teams[0].roster.roster;
-      // Push a promise for each player on the roster to fetch their info to array
+      // Push a promise to array for each player on the roster to fetch their info
       result.forEach((data) =>
         playerInfoPromises.push(
           axios.get(
@@ -35,7 +36,7 @@ const RosterTable = () => {
           )
         )
       );
-      // Push a promise for each player on the roster to fetch their stats to array
+      // Push a promise to array for each player on the roster to fetch their stats
       result.forEach((data) =>
         playerStatsPromises.push(
           axios.get(
@@ -57,13 +58,73 @@ const RosterTable = () => {
         setPlayerStats(data);
       };
       getPlayerStats();
-    })
+    });
   }, [url]);
 
+  // Create array and loop through team roster and push a <tr> for each player
+  // to hold their info and stats
+  let rows = [];
   if (playerInfo && playerStats) {
-      let playerData = [playerInfo, playerStats];
-      //console.log(playerData);
+    for (let i = 0; i < playerInfo.length; i++) {
+      rows.push(
+        <tr key={parseInt(playerInfo[i].id)}>
+          {/* Name */}
+          <td>{playerInfo[i].fullName}</td>
+          {/* # */}
+          <td>{playerInfo[i].primaryNumber}</td>
+          {/* Position */}
+          <td>{playerInfo[i].primaryPosition.code}</td>
+          {/* Shot (Left or Right) */}
+          <td>{playerInfo[i].shootsCatches}</td>
+          {/* Height */}
+          <td>{playerInfo[i].height}</td>
+          {/* Weight */}
+          <td>{playerInfo[i].weight}</td>
+          {/* Birthdate */}
+          <td>{playerInfo[i].birthDate}</td>
+          {/* Birthplace */}
+          {playerInfo[i].birthStateProvince ? (
+            <td>{`${playerInfo[i].birthCity}, ${playerInfo[i].birthStateProvince}, ${playerInfo[i].birthCountry}`}</td>
+          ) : (
+            <td>{`${playerInfo[i].birthCity}, ${playerInfo[i].birthCountry}`}</td>
+          )}
+          {/* GP (Games Played) */}
+          <td>{playerStats[i] ? playerStats[i].stat.games : "N/A"}</td>
+          {/* G (Goals) */}
+          <td>{playerStats[i] ? playerStats[i].stat.goals : "N/A"}</td>
+          {/* A (Assists) */}
+          <td>{playerStats[i] ? playerStats[i].stat.assists : "N/A"}</td>
+          {/* P (Points) */}
+          <td>{playerStats[i] ? playerStats[i].stat.points : "N/A"}</td>
+          {/* +/- (plus/minus) */}
+          <td>{playerStats[i] ? playerStats[i].stat.plusMinus : "N/A"}</td>
+          {/* PiM (Penalty in Minutes) */}
+          <td>{playerStats[i] ? playerStats[i].stat.penaltyMinutes : "N/A"}</td>
+          {/* Hits */}
+          <td>{playerStats[i] ? playerStats[i].stat.hits : "N/A"}</td>
+          {/* ToI (Total Time on Ice) */}
+          <td>{playerStats[i] ? playerStats[i].stat.timeOnIce : "N/A"}</td>
+          {/* ToI/G (Time on Ice per Game) */}
+          <td>{playerStats[i] ? playerStats[i].stat.timeOnIcePerGame : "N/A"}</td>
+          {/* ToI/SH (Time on Ice Short Handed per Game) */}
+          <td>{playerStats[i] ? playerStats[i].stat.shortHandedTimeOnIcePerGame : "N/A"}</td>
+          {/* ToI/PP (Time on Ice Powerplay per Game) */}
+          <td>{playerStats[i] ? playerStats[i].stat.powerPlayTimeOnIcePerGame : "N/A"}</td>
+          {/* PPG (Powerplay Goals) */}
+          <td>{playerStats[i] ? playerStats[i].stat.powerPlayGoals : "N/A"}</td>
+          {/* PPP (Powerplay Points) */}
+          <td>{playerStats[i] ? playerStats[i].stat.powerPlayPoints : "N/A"}</td>
+          {/* FO% (Faceoff Percentage) */}
+          <td>{playerStats[i] ? playerStats[i].stat.faceOffPct : "N/A"}</td>
+          {/* S% (Shooting Percentage) */}
+          <td>{playerStats[i] ? playerStats[i].stat.shotPct : "N/A"}</td>
+          {/* SB (Shots Blocked) */}
+          <td>{playerStats[i] ? playerStats[i].stat.blocked : "N/A"}</td>
+        </tr>
+      );
+    }
   }
+
 
   return (
     <Container
@@ -79,7 +140,7 @@ const RosterTable = () => {
       {/* Container to display rosters */}
       <Container
         height="35rem"
-        width="80%"
+        width="90%"
         backgroundColor="rgb(255,255,255, 0.8)"
         margin="1rem 0 0 0"
         overflowX="auto"
@@ -114,65 +175,7 @@ const RosterTable = () => {
             </tr>
           </thead>
           <tbody>
-            {playerInfo && playerStats 
-              ? playerInfo.map((player) => (
-                  <tr key={parseInt(player.id)}>
-                    {/* Name */}
-                    <td>{player.fullName}</td>
-                    {/* # */}
-                    <td>{player.primaryNumber}</td>
-                    {/* Position */}
-                    <td>{player.primaryPosition.code}</td>
-                    {/* Shot (Left or Right) */}
-                    <td>{player.shootsCatches}</td>
-                    {/* Height */}
-                    <td>{player.height}</td>
-                    {/* Weight */}
-                    <td>{player.weight}</td>
-                    {/* Birthdate */}
-                    <td>{player.birthDate}</td>
-                    {/* Birthplace */}
-                    {player.birthStateProvince ? (
-                      <td>{`${player.birthCity}, ${player.birthStateProvince}, ${player.birthCountry}`}</td>
-                    ) : (
-                      <td>{`${player.birthCity}, ${player.birthCountry}`}</td>
-                    )}
-
-                    {/* GP (Games Played) */}
-                    <td></td>
-                    {/* G (Goals) */}
-                    <td></td>
-                    {/* A (Assists) */}
-                    <td></td>
-                    {/* P (Points) */}
-                    <td></td>
-                    {/* +/- (plus/minus) */}
-                    <td></td>
-                    {/* PiM (Penalty in Minutes) */}
-                    <td></td>
-                    {/* Hits */}
-                    <td></td>
-                    {/* ToI (Total Time on Ice) */}
-                    <td></td>
-                    {/* ToI/G (Time on Ice per Game) */}
-                    <td></td>
-                    {/* ToI/SH (Time on Ice Short Handed per Game) */}
-                    <td></td>
-                    {/* ToI/PP (Time on Ice Powerplay per Game) */}
-                    <td></td>
-                    {/* PPG (Powerplay Goals) */}
-                    <td></td>
-                    {/* PPP (Powerplay Points) */}
-                    <td></td>
-                    {/* FO% (Faceoff Percentage) */}
-                    <td></td>
-                    {/* S% (Shooting Percentage) */}
-                    <td></td>
-                    {/* SB (Shots Blocked) */}
-                    <td></td>
-                  </tr>
-                ))
-              : null}
+            {rows ? rows : "Error while loading data"}
             ;
           </tbody>
         </Table>
